ffce62c7ee441a81b7cae71bb2d3b6dda0d050e7
king30milly <119558354+king30milly@users.noreply.github.com>
2024-01-04 21:03:28 -0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,14 +2,14 @@ Config = {}
 
 Config.Locale = 'en'
 
-Config.Bandage = 'Bandage'  --Can change to an equivalent item in your database or run the items.sql, icon in items folder, remember case sensitive
-Config.Revive = 'Morphine' --Can change to an equivalent item in your database or run the items.sql, icon in items folder, remember case sensitive
-Config.usewebhook = false
-Config.Webhook = ''
+Config.Bandage = 'bandage'  --Can change to an equivalent item in your database or run the items.sql, icon in items folder, remember case sensitive
+Config.Revive = 'morphine' --Can change to an equivalent item in your database or run the items.sql, icon in items folder, remember case sensitive
+Config.usewebhook = true
+Config.Webhook = 'https://discord.com/api/webhooks/1192669796634988605/FLz8NKQspAcdt4YmNDYFraNOIBTdZA2OTMxavp_dj7CNTTkx6-T6f0uYVomw5DYAjzjg'
 Config.Command = 'medic' -- Slash command to use in chat to open Medic Menu
 Config.gonegative = true -- Can you go negative paying for NPC revival
-Config.synsociety = false
-Config.playerscantuseband = true -- set false if you want players to use bandages them self 
+Config.synsociety = true
+Config.playerscantuseband = false -- set false if you want players to use bandages them self 
 
 Config.helthcheck = 300
 
@@ -31,13 +31,13 @@ Doctoroffices = {
 
 MedicJobs = {
     "doctor", -- Jobs that count as Doctors
-    "police"
+
 }
 
 
 Config.doctors = {
     ped = 
-    "mp_u_m_m_nat_farmer_01", -- Model of NPC Doctor or choose other ped model below
+    "am_valentinedoctors_females_01", -- Model of NPC Doctor or choose other ped model below
 
   			--am_valentinedoctors_females_01
 			--cs_sddoctor_01
@@ -47,7 +47,7 @@ Config.doctors = {
   
     command = "sendhelp", -- Command to Call for NPC Doctor
 
-    amount = 45, -- Payment for Revive from NPC Doctor
+    amount = 35, -- Payment for Revive from NPC Doctor
 
     timer = 60000 * 1 -- put how many minutes you'd like ie 60000 * 5 for 5 minutes
 	
